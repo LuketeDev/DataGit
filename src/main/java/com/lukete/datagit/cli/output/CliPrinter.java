65f83e38563afcb7782aa522db050647ef4eba92
LuketeDev <lulukas.2023@gmail.com>
2026-04-18 19:00:22 -0300

    feat(printer): create better cli logging

---
+++ src/main/java/com/lukete/datagit/cli/output/CliPrinter.java
@@ -0,0 +1,36 @@
+package com.lukete.datagit.cli.output;
+
+import java.io.PrintWriter;
+import java.security.MessageDigest;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CliPrinter {
+    private final PrintWriter out;
+    private final PrintWriter err;
+
+    public void success(String message) {
+        out.println(message);
+    }
+
+    public void info(String message) {
+        out.println("INFO " + message);
+    }
+
+    public void warn(String message) {
+        err.println("WARN " + message);
+    }
+
+    public void error(String message) {
+        err.println("ERROR " + message);
+    }
+
+    public void hint(String message) {
+        out.println("TIP " + message);
+    }
+
+    public void blankLine() {
+        out.println();
+    }
+}
